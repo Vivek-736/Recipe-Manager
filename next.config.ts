bcd5d534fa5d6d9f84fa2d8af64f858fb844ac36
Vivek <vivekousik8555@gmail.com>
2024-12-31 15:26:14 +0530

    bas

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['external-content.duckduckgo.com', 'cravingzone.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
-  {
-  protocol: 'https',
-    hostname: 'avatars.githubusercontent.com',
-},
-{
-  protocol: 'https',
-    hostname: 'lh3.googleusercontent.com',
-}
 };
 
 export default nextConfig;
